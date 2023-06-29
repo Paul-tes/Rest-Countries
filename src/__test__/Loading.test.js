1b3e90dc4eff7f341e48b6653bce69c84eba06f7
paul-tes <pauasca8462@gmail.com>
2023-06-29 17:50:56 +0300

    Loading component test

--- src/__test__/Loading.test.js
+++ src/__test__/Loading.test.js
@@ -1,13 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import Loading from '../components/Loading';
+import '@testing-library/jest-dom';
 
 describe('Loading', () => {
   it('should render the loading component', () => {
     render(<Loading />);
 
-    expect(screen.getByRole('section')).toBeInTheDocument();
-    expect(screen.getByRole('div')).toBeInTheDocument();
-    expect(screen.getByRole('section')).toHaveClass('sec-loading');
-    expect(screen.getByRole('div')).toHaveClass('one');
+    expect(screen.getByTestId('loading-section')).toBeInTheDocument();
+    expect(screen.getByTestId('loading-div')).toBeInTheDocument();
+    expect(screen.getByTestId('loading-section')).toHaveClass('sec-loading');
+    expect(screen.getByTestId('loading-div')).toHaveClass('one');
   });
 });
